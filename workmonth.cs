60a65c3328f2e5def720eef4fcdbd7817b221d33
unknown <toghrul@code.edu.az>
2022-08-02 16:59:00 +0400

    workmonth

---
+++ workmonth.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace work_month
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = "yanvar";
+            switch  (month)
+            {
+                case "yanvar": case "mart": case "may": case "iyul": case "avqust": case "oktyabr": case "dekabr":
+                    Console.WriteLine(31);
+                    break;
+                case "aprel": case "iyun": case "sentyabr": case "noyabr":
+                    Console.WriteLine(30);
+                    break;
+                case "fevral":
+                    Console.WriteLine(28);
+                    break;
+            }
+        }
+    }
+}
